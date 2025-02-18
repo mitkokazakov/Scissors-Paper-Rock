1a568dca072db43936643e2b02db477f841f7c0a
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2025-02-18 13:35:53 +0200

    Style computer's choice div to pop up when user select some of three options

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ let scissors = document.querySelector('.scissors');
 let paper = document.querySelector('.paper');
 let rock = document.querySelector('.rock');
 
+let threeChoicesBg = document.querySelector('.three-choices-game-bg')
+
+let computerChoice = document.querySelector('.computer-choice');
+
 scissors.addEventListener('click', () => {
     scissors.classList.add('selected')
+    paper.classList.add('hidden');
+    rock.classList.add('hidden');
+    threeChoicesBg.classList.add('hidden');
+    computerChoice.classList.add('selected');
 })
 
 paper.addEventListener('click', () => {
