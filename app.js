553f312ecce845a166675adc44ecc270b2fd58bd
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2025-02-20 12:44:07 +0200

    Add animations to move separate choices by user and computer

--- app.js
+++ app.js
@@ -6,18 +6,46 @@ let threeChoicesBg = document.querySelector('.three-choices-game-bg')
 
 let computerChoice = document.querySelector('.computer-choice');
 
+let myChoiceText = document.querySelector('.my-choice-text');
+let computerChoiceText = document.querySelector('.computer-choice-text');
+
+let gameResult = document.querySelector('.game-result');
+
+let gameResultWrapper = document.querySelector('.game-result-wrapper');
+
 scissors.addEventListener('click', () => {
     scissors.classList.add('selected')
     paper.classList.add('hidden');
     rock.classList.add('hidden');
     threeChoicesBg.classList.add('hidden');
     computerChoice.classList.add('selected');
+    myChoiceText.classList.add('selected');
+    computerChoiceText.classList.add('selected')
+    gameResult.classList.add('selected')
+    gameResultWrapper.classList.add('selected')
 })
 
 paper.addEventListener('click', () => {
-    paper.classList.add('selected')
+    paper.classList.add('selected');
+    scissors.classList.add('hidden')
+    rock.classList.add('hidden');
+    threeChoicesBg.classList.add('hidden');
+    computerChoice.classList.add('selected');
+    myChoiceText.classList.add('selected');
+    computerChoiceText.classList.add('selected')
+    gameResult.classList.add('selected')
+    gameResultWrapper.classList.add('selected')
+
 })
 
 rock.addEventListener('click', () => {
     rock.classList.add('selected')
+    scissors.classList.add('hidden')
+    paper.classList.add('hidden');
+    threeChoicesBg.classList.add('hidden');
+    computerChoice.classList.add('selected');
+    myChoiceText.classList.add('selected');
+    computerChoiceText.classList.add('selected')
+    gameResult.classList.add('selected')
+    gameResultWrapper.classList.add('selected')
 })
