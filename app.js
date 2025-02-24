95a5c22a8e40293766571ed1aa5845cec228bbcd
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2025-02-24 13:33:29 +0200

    Add logic for automatically pick up a choices

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let a = document.querySelector('.choice.paper');
+let paper = document.querySelector('.choice.paper');
+let rock = document.querySelector('.choice.rock');
+let scissors = document.querySelector('.choice.scissors');
 let gameContainer = document.querySelector('.game')
 let resultsContainer = document.querySelector('.results')
 let computerResult = document.querySelector('.result.computer-result .choice');
@@ -14,19 +16,67 @@ let computerText = document.querySelector('.text.computer-text');
 
 let finalResult = document.querySelector('.final-result');
 
-a.addEventListener('click', () => {
+let playAgainBtn = document.querySelector('.play-again');
+
+paper.addEventListener('click', () => {
+    AddUserChoice('paper')
+    ToggleClasses();
+})
+rock.addEventListener('click', () => {
+    AddUserChoice('rock')
+    ToggleClasses();
+})
+scissors.addEventListener('click', () => {
+    AddUserChoice('scissors')
+    ToggleClasses();
+})
+
+playAgainBtn.addEventListener('click', ToggleClasses)
+
+let choices = [
+    {
+        name: 'paper',
+        beats: 'rock'
+    },
+    {
+        name: 'rock',
+        beats: 'scissors'
+    },
+    {
+        name: 'scissors',
+        beats: 'paper'
+    }
+]
+
+function ToggleClasses(){
+    
+    AddComputerChoice();
     gameContainer.classList.toggle('hidden')
     resultsContainer.classList.toggle('hidden')
     computerChoiceWrapper.classList.toggle('shown');
-    // darkCircle.classList.toggle('hidden')
-
-
     userResult.classList.toggle('left');
     userResultText.classList.toggle('left-text');
-
     computerContainerToMove.classList.toggle('right');
     computerText.classList.toggle('right-text');
-
     finalResult.classList.toggle('show-final');
+}
 
-})
+function AddUserChoice(choice){
+    userResult.innerHTML = `<section class="choice ${choice}">
+            <img src="./images/icon-${choice}.svg" alt="" srcset="">
+          </section>`
+}
+
+function AddComputerChoice(){
+
+    let randomChoice = Math.round(Math.random() * 2);
+
+    let curentChoice = choices[randomChoice];
+    
+    computerChoiceWrapper.innerHTML = `<section class="choice ${curentChoice.name}">
+              <img src="./images/icon-${curentChoice.name}.svg" alt="" srcset="">
+            </section>`
+}
+
+
+
