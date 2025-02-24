8040a82fa9db2021c903350b7a74f2613470c1a6
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2025-02-24 10:53:22 +0200

    Add animations to move the user and computer's choice

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 let a = document.querySelector('.choice.paper');
-let game = document.querySelector('.game')
-let res = document.querySelector('.results')
+let gameContainer = document.querySelector('.game')
+let resultsContainer = document.querySelector('.results')
+let computerResult = document.querySelector('.result.computer-result .choice');
+let darkCircle = document.querySelector('.dark-circle')
+
+let computerChoiceWrapper = document.querySelector('.computer-choice-wrapper')
+
+let userResult = document.querySelector('.result.user-result');
+let userResultText = document.querySelector('.text.user-text');
+
+let computerContainerToMove = document.querySelector('.result.computer-result');
+let computerText = document.querySelector('.text.computer-text');
+
+let finalResult = document.querySelector('.final-result');
 
 a.addEventListener('click', () => {
-    game.classList.toggle('hidden')
-    res.classList.toggle('hidden')
+    gameContainer.classList.toggle('hidden')
+    resultsContainer.classList.toggle('hidden')
+    computerChoiceWrapper.classList.toggle('shown');
+    // darkCircle.classList.toggle('hidden')
+
+
+    userResult.classList.toggle('left');
+    userResultText.classList.toggle('left-text');
+
+    computerContainerToMove.classList.toggle('right');
+    computerText.classList.toggle('right-text');
+
+    finalResult.classList.toggle('show-final');
+
 })
